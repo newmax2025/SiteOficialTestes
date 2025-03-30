2b487dab0311edb9bb99f9fbd45b16592fd8d520
newmax2025 <newmaxconsultas@gmail.com>
2025-03-30 11:31:15 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 header('Content-Type: application/json');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 
 // Importa a configuração do banco
 require 'config.php';
