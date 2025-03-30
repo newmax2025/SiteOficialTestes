42c27a2e750cf03127bd94270e1b4959b313e8d4
newmax2025 <newmaxconsultas@gmail.com>
2025-03-30 00:23:52 -0300

    ajuste

--- teste/api.php
+++ teste/api.php
@@ -1,8 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Access-Control-Allow-Headers: Content-Type");
-
+header("Content-Type: application/json");
 
 require 'config.php';
 $conexao = new mysqli($host, $username, $password, $dbname);
