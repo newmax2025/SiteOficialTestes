db84a5522de76e31074fb9367eab9f90325d03ae
newmax2025 <newmaxconsultas@gmail.com>
2025-04-05 17:27:02 -0300

    Update aM.php

--- public/views/aM.php
+++ public/views/aM.php
@@ -56,7 +56,7 @@
     </ul>
     </div>
     <header>
-         <a href="../assets/views/aM.php"> <img src="../assets/img/New Max Buscas.png" alt="New Max Buscas.png"></a>
+         <a href="../public/views/aM.php"> <img src="../assets/img/New Max Buscas.png" alt="New Max Buscas.png"></a>
     </header>
      <!-- código do popup -->
 
