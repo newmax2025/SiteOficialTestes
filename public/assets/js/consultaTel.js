74fd6a4d55beba845fb493bde2cddad356c4132c
newmax2025 <newmaxconsultas@gmail.com>
2025-04-07 12:44:12 -0300

    Update consultaTel.js

--- public/assets/js/consultaTel.js
+++ public/assets/js/consultaTel.js
@@ -125,8 +125,8 @@ function formatarTelefone(tel) {
 
 function formatTel(input) {
   let value = input.value.replace(/\D/g, "");
-  value = value.replace(/(\d{3})(\d)/, "$1.$2");
-  value = value.replace(/(\d{3})(\d)/, "$1.$2");
-  value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  value = value.replace(/^(\d{2})(\d)/g, "($1) $2");
+  value = value.replace(/(\d{5})(\d)/, "$1-$2");
+
   input.value = value;
 }
