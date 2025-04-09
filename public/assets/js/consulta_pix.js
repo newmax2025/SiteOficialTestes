64843f59fdbccc78a19f58da0c23531c0f37b4ac
newmax2025 <newmaxconsultas@gmail.com>
2025-04-09 11:02:45 -0300

    Update consulta_pix.js

--- public/assets/js/consulta_pix.js
+++ public/assets/js/consulta_pix.js
@@ -120,18 +120,3 @@ function consultarCPF() {
       resetCaptcha();
     });
 }
-
-
-
-function formatarCPF(cpf) {
-  if (!cpf) return "";
-  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-}
-
-function formatCPF(input) {
-  let value = input.value.replace(/\D/g, "");
-  value = value.replace(/(\d{3})(\d)/, "$1.$2");
-  value = value.replace(/(\d{3})(\d)/, "$1.$2");
-  value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
-  input.value = value;
-}
