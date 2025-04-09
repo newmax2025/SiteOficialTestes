e7e0b222f98c705af94ec37a4539dc68ab8147fd
newmax2025 <newmaxconsultas@gmail.com>
2025-04-09 14:42:07 -0300

    Update consultaCNPJ.php

--- public/views/consultaCNPJ.php
+++ public/views/consultaCNPJ.php
@@ -49,7 +49,7 @@
         </div>
 
     </div>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
     <script src="../assets/js/consultaCNPJ.js?v=<?php echo md5_file('../assets/js/consultaCNPJ.js'); ?>"></script>
 </body>
