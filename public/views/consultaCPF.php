1f65a58e7728ffeea6afb0a4a20bf39a3cf2380b
newmax2025 <newmaxconsultas@gmail.com>
2025-04-09 13:46:25 -0300

    Update consultaCPF.php

--- public/views/consultaCPF.php
+++ public/views/consultaCPF.php
@@ -44,10 +44,11 @@
     </div>
 
      <div id="dados" class="dados" style="display: none;">
-            <!-- Botão para baixar PDF -->
-            <button id="baixarPDFBtn" onclick="baixarPDF()" style="margin-top: 15px;">Baixar PDF</button>
+            
         </div>
     </div>
+    <!-- Botão para baixar PDF -->
+            <button id="baixarPDFBtn" onclick="baixarPDF()" style="margin-top: 15px;">Baixar PDF</button>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
