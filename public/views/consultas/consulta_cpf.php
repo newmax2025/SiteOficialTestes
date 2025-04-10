0c999b16b96e8c9889c9c37aacff99c5bbfce82a
newmax2025 <newmaxconsultas@gmail.com>
2025-04-10 19:23:45 -0300

    atualizando referencia

--- public/views/consultas/consulta_cpf.php
+++ public/views/consultas/consulta_cpf.php
@@ -4,22 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Painel de Consulta CNPJ</title>
+    <title>Painel de Consulta CPF</title>
     <link rel="stylesheet" href="../assets/css/consultaCPF.css?v=<?php echo md5_file('../assets/css/consultaCPF.css'); ?>">
     <script>
         fetch("../backend/verifica_sessao.php")
-    .then(response => response.json())
-    .then(data => {
-        if (!data.autenticado) {
-            window.location.href = "login.php"; // Redireciona se não estiver autenticado
-        }
-    })
-    .catch(error => {
-        console.error("Erro ao verificar sessão:", error);
-        window.location.href = "login.php"; // Opcional: Redireciona em caso de erro
-    });
-
-        </script>
+            .then(response => response.json())
+            .then(data => {
+                if (!data.autenticado) {
+                    window.location.href = "login.php";
+                }
+            })
+            .catch(error => {
+                console.error("Erro ao verificar sessão:", error);
+                window.location.href = "login.php";
+            });
+    </script>
 </head>
 
 <body>
@@ -27,14 +26,11 @@
         <div class="logo-container">
             <img class="logo" src="../assets/img/New Max Buscas.png" alt="Logo do Cliente">
         </div>
-        <h2>Consulta CNPJ</h2>
-        <input type="text" id="cnpj" placeholder="Digite o CNPJ" maxlength="18" oninput="formatCNPJ(this)">
-        <button id="consultarBtn" onclick="consultarCNPJ()" disabled>Consultar</button>
+        <h2>Consulta CPF</h2>
+        <input type="text" id="cpf" placeholder="Digite o CPF" maxlength="14" oninput="formatCPF(this)">
+        <button id="consultarBtn" onclick="consultarCPF()" disabled>Consultar</button>
 
-        <!-- Turnstile CAPTCHA -->
-        <div class="cf-turnstile" id="captcha" data-sitekey="0x4AAAAAABDPzCDp7OiEAfvh" data-callback="onCaptchaSuccess">
-        </div>
-
+        <div class="cf-turnstile" id="captcha" data-sitekey="0x4AAAAAABDPzCDp7OiEAfvh" data-callback="onCaptchaSuccess"></div>
         <input type="hidden" id="captcha-response" name="cf-turnstile-response">
 
         <p id="resultado"></p>
@@ -47,11 +43,11 @@
             <button style="margin-top: 20px;" onclick="baixarPDF()">Baixar em PDF</button>
             <button style="margin-top: 20px;" onclick="baixarTXT()">Baixar em TXT</button>
         </div>
-
     </div>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-    <script src="../assets/js/consultaCNPJ.js?v=<?php echo md5_file('../assets/js/consultaCNPJ.js'); ?>"></script>
+    <script src="../assets/js/consulta_cpf.js?v=<?php echo md5_file('../assets/js/consulta_cpf.js'); ?>"></script>
     <script src="../assets/js/baixar_consultas.js?v=<?php echo md5_file('../assets/js/baixar_consultas.js'); ?>"></script>
 </body>
 
