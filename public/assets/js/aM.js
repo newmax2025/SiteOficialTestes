32f5c5622f9851a58dc14d65639a385d0dcec5e4
newmax2025 <newmaxconsultas@gmail.com>
2025-04-11 01:23:58 -0300

    Update aM.js

--- public/assets/js/aM.js
+++ public/assets/js/aM.js
@@ -46,34 +46,15 @@ document.querySelectorAll('.carousel-container').forEach(container => {
         isDown = false;
     });
 
-    document.addEventListener("DOMContentLoaded", function () {
-  const container = document.getElementById("carousel-container");
-
+   document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("carousel-container2");
   if (!container) return;
 
   const cards = Array.from(container.children);
-
-  // Duplicar os cards
   cards.forEach(card => {
     const clone = card.cloneNode(true);
     container.appendChild(clone);
   });
-
-  let scrollAmount = 0;
-
-  function animateCarousel() {
-    scrollAmount += 1;
-    container.scrollLeft = scrollAmount;
-
-    if (scrollAmount >= container.scrollWidth / 2) {
-      scrollAmount = 0;
-      container.scrollLeft = 0;
-    }
-
-    requestAnimationFrame(animateCarousel);
-  }
-
-  animateCarousel();
 });
-});
+
 
