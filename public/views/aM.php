826d7c24891c71c197cf8e5caf2c04268b947b33
newmax2025 <newmaxconsultas@gmail.com>
2025-04-11 00:44:05 -0300

    Update aM.php

--- public/views/aM.php
+++ public/views/aM.php
@@ -466,8 +466,8 @@
               
  const banner = document.querySelector('.banner-grande');
   const imagens = [
-    '../assets/img/banner_new_MAX.png',
-    '../assets/img/assine_o_plano_premium2.png'
+    '../assets/img/banner_principal1..jpg',
+    '../assets/img/assine_o_plano_premium.jpg'
     
   ];
 
