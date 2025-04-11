18be34e80620a5cfdb8269be0e4ae9262ad06143
newmax2025 <newmaxconsultas@gmail.com>
2025-04-11 00:39:07 -0300

    Update aM.php

--- public/views/aM.php
+++ public/views/aM.php
@@ -103,15 +103,15 @@
     <section class="carousel" id="favoritos">
         <h2>Favoritos</h2>
         <div class="carousel-container">
-             <div class="card grande"> <img src="../assets/img/CRLV_digital_horizontal.jpg" alt="CRLV DIGITAL Horizontal"> </div>
-            <div class="card grande"> <img src="../assets/img/Impressão_CNH_horizontal.jpg" alt="Impressão CNH Horizontal"> </div>
-            <div class="card grande"> <img src="../assets/img/consulta_SERASA_horizontal.jpg" alt="Consulta SERASA Horizontal"> </div>
-            <div class="card grande"> <img src="../assets/img/SCPC_horizontal.jpg" alt="SCPC Horizontal"> </div>
-            <div class="card grande"> <img src="../assets/img/CNH_completa_com_foto_horizontal.jpg" alt="CNH Completa com Foto Horizontal"> </div>
-            <div class="card grande"> <img src="../assets/img/consulta_radar_de_veiculos_horizontal.jpg" alt="CONSULTA RADAR DE VEÍCULOS Horizontal"> </div>
-            <div class="card grande"> <img src="../assets/img/consulta_detran_pro.jpg" alt="CONSULTA Detran Pro Horizontal"> </div>
-            <div class="card grande"> <img src="../assets/img/placa_plus_plano_premium_horinzontal.jpg" alt="Placa Plus Plano Premium Horinzontal"> </div>
-            <div class="card grande"> <img src="../assets/img/consulta_credi_link_horizontal.jpg" alt="Consulta  CREDI LINK Horizontal"> </div>
+             <div class="card grande"> <img src="../assets/img_banner/CRLV_digital_horizontal.jpg" alt="CRLV DIGITAL Horizontal"> </div>
+            <div class="card grande"> <img src="../assets/img_banner/Impressão_CNH_horizontal.jpg" alt="Impressão CNH Horizontal"> </div>
+            <div class="card grande"> <img src="../assets/img_banner/consulta_SERASA_horizontal.jpg" alt="Consulta SERASA Horizontal"> </div>
+            <div class="card grande"> <img src="../assets/img_banner/SCPC_horizontal.jpg" alt="SCPC Horizontal"> </div>
+            <div class="card grande"> <img src="../assets/img_banner/CNH_completa_com_foto_horizontal.jpg" alt="CNH Completa com Foto Horizontal"> </div>
+            <div class="card grande"> <img src="../assets/img_banner/consulta_radar_de_veiculos_horizontal.jpg" alt="CONSULTA RADAR DE VEÍCULOS Horizontal"> </div>
+            <div class="card grande"> <img src="../assets/img_banner/consulta_detran_pro.jpg" alt="CONSULTA Detran Pro Horizontal"> </div>
+            <div class="card grande"> <img src="../assets/img_banner/placa_plus_plano_premium_horinzontal.jpg" alt="Placa Plus Plano Premium Horinzontal"> </div>
+            <div class="card grande"> <img src="../assets/img_banner/consulta_credi_link_horizontal.jpg" alt="Consulta  CREDI LINK Horizontal"> </div>
             <!-- Adicione mais cards conforme necessário -->
         </div>
     </section>
