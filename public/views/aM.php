b6ca7e85ae4c7f1d4237f570c9559ee089773d8a
newmax2025 <newmaxconsultas@gmail.com>
2025-04-11 14:19:29 -0300

    Update aM.php

--- public/views/aM.php
+++ public/views/aM.php
@@ -56,11 +56,11 @@ if (!isset($_SESSION["usuario"])) {
     <div class="sidebar">
     <header>Menu</header>
     <ul>
-        <li><a href="#"><i class=""></i>Perfil 🔐</a></li>
+        <li><a href="usuario_pag.php"><i class=""></i>Perfil 🔐</a></li>
         <li><a href="#" id="revendedor"><i class=""></i>Revendedor: Carregando...</a></li>
         <li><a href="https://wa.me/" id="whatsapp"><i class="fa-brands fa-whatsapp"></i>Whatsapp</a></li>
         <li><a href="#" id="plano"><i class=""></i>Plano: Carregando...</a></li>
-        <li><a href="usuario_pag.php" id="saldo"><i class=""></i>Saldo: Carregando...</a></li>
+        <li><a href="#" id="saldo"><i class=""></i>Saldo: Carregando...</a></li>
         <a href="../backend/logout.php">Sair</a>
     </ul>
     </div>
