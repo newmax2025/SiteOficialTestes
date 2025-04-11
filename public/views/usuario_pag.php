d2d3ec587285ecd8c57bfa15915168fc5b899406
newmax2025 <newmaxconsultas@gmail.com>
2025-04-11 14:35:30 -0300

    adicionando css

--- public/views/usuario_pag.php
+++ public/views/usuario_pag.php
@@ -13,10 +13,6 @@ if (!isset($_SESSION['usuario'])) {
     <title>Página do Usuário</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/usuario_pag.css?v=<?php echo md5_file('../assets/css/usuario_pag.css'); ?>">
-    <style>
-        
-
-    </style>
 </head>
 <body>
     <div class="sidebar">
